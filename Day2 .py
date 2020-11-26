b51def6ee514d2bedf4374b28f0fbabdc8204e7b
christikas <74914223+christikas@users.noreply.github.com>
2020-11-26 19:21:10 +0530

    Add files via upload

---
+++ Day2 .py
@@ -0,0 +1,21 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=30
+>>> b=6
+>>> c=60
+>>> print(a/10)
+3.0
+>>> print(b*50)
+300
+>>> print(c+60)
+120
+>>> x="Marie"
+>>> print(x.replace("r","G"))
+MaGie
+>>> a=10
+>>> b=43.5
+>>> print(int(b))
+43
+>>> print(float(a))
+10.0
+>>> 
